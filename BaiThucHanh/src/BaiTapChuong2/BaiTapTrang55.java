089170efc209dd47287fef64c482103e469175ea
TranHuyHoang69 <tranhuyhoangwork69@gmail.com>
2024-05-14 19:02:19 +0700

    Bài thực hành chương 2 slide 55

---
+++ BaiThucHanh/src/BaiTapChuong2/BaiTapTrang55.java
@@ -0,0 +1,11 @@
+package BaiTapChuong2;
+
+public class BaiTapTrang55 {
+    public static void main(String[] args) {
+        for (int i=1; i<=20;i++){
+            if(i%5==0){
+                System.out.println(i);
+    }
+  }
+ }
+}
