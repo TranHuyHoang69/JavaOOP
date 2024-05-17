826844fb41be8c72ae45a80435dc78029c7c3cd8
TranHuyHoang69 <tranhuyhoangwork69@gmail.com>
2024-05-17 18:16:29 +0700

    Bài thực hành chương 4 HashSet slide 56

--- BaiThucHanh/src/BaiTapChuong4/ArrayList/Sets/HashSetDemo.java
+++ BaiThucHanh/src/BaiTapChuong4/ArrayList/Sets/HashSetDemo.java
@@ -1,15 +1,33 @@
 package BaiTapChuong4.ArrayList.Sets;
 
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class HashSetDemo {
     public static void main(String[] args) {
-        HashSet<StudentSet> hashSetStudent = new HashSet<StudentSet>();
-        for(int i = 0; i<3;i++){
-            StudentSet st = new StudentSet();
-            st.In();
-            hashSetStudent.add(st);
+         String name ;
+        HashSet<String> hset = new HashSet<String>();
+        Scanner sc = new Scanner(System.in);
+        hset.add("Iphone 15 pro max");
+        hset.add("Samsung galaxy s22 ultra");
+        hset.add("Oppo Reno 8");
+        hset.add("Xiaomi redmi 12");
+        hset.add("Vivo x90");
+        hset.add("Huawei pura 70");
+        hset.add("Realme q3 pro");
+        hset.add("Lenovo thinkpad");
+        hset.size();
+        System.out.println("Cac phan tu trong HashSetString : ");
+        System.out.println(hset);
+        System.out.println("Nhap cac phan tu can xoa ");
+        name = sc.nextLine();
+        if(hset.contains(name)){
+            hset.remove(name);
+            System.out.println("Xoa thanh cong ");
+            System.out.println("Cac phan tu con lai trong HashSetString : ");
+            System.out.println(hset);
+        }else{
+            System.out.println("Xoa khong thanh cong ");
         }
-        System.out.println(hashSetStudent);
     }
 }
