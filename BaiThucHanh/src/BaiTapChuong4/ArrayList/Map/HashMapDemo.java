ef670c64cb726d1267f415da68356282fe804331
TranHuyHoang69 <tranhuyhoangwork69@gmail.com>
2024-05-18 19:40:25 +0700

    Bài thực hành chương 4 HashMap slide 86

---
+++ BaiThucHanh/src/BaiTapChuong4/ArrayList/Map/HashMapDemo.java
@@ -0,0 +1,19 @@
+package BaiTapChuong4.ArrayList.Map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<String, String>();
+        hashMap.put("1 + 1 ", " 2");
+        hashMap.put("6 + 9 ", " 15");
+        hashMap.put("10 + 11 ", " 21");
+        hashMap.put("20 + 22 ", " 42"); 
+        hashMap.put("9 + 10 ", " 21");
+        Set<Map.Entry<String, String>> setHashMap = hashMap.entrySet(); 
+        System.out.println("Cac entry trong HashMap : ");
+        System.out.println(setHashMap);
+    }
+}
